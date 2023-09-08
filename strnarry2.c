2a2c92b83eade46f5a955ecca1bca5b5f14aff09
CletsyMedia <Cletussam12@yahoo.com>
2023-09-08 20:01:23 +0100

    Strnarry2 scroll

--- strnarry2.c
+++ strnarry2.c
@@ -1,1 +1,13 @@
 #include "shellux.h"
+
+/**
+ * _putchar - Write a character to the standard output.
+ * @ch: The character to be written.
+ *
+ * This function writes a single character to the standard output.
+ * If the internal buffer is full or if a special flush character
+ * is encountered, the content of the buffer is flushed to the output.
+ *
+ * Return: On success, 1. On error, -1 is returned, and errno is set
+ *	appropriately.
+ */

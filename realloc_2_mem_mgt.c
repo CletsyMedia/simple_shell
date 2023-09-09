b8f006b794b8813a25b922da16854a848cbe3617
CletsyMedia <Cletussam12@yahoo.com>
2023-09-09 14:59:53 +0100

    Unlocking memories

--- realloc_2_mem_mgt.c
+++ realloc_2_mem_mgt.c
@@ -8,3 +8,17 @@
  * its individual strings and the array itself. It takes a char ** pointer as
  * input, iterates through the strings, and releases memory to prevent leaks.
  */
+void free_mem(char **pp)
+{
+	char **a = pp; /* Create a copy of the original pointer */
+
+	if (!pp) /* If `pp` is NULL, nothing to free */
+		return;
+	/* Loop until a pointer in the array is NULL (end of array) */
+	for (; *pp; pp++)
+	{
+		free(*pp); /* Free the memory pointed to by the current pointer */
+	}
+
+	free(a); /* Free the memory of the array itself */
+}

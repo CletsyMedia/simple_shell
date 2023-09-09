d55e5a622043a94b0393ac244f7695731a4d19cb
CletsyMedia <Cletussam12@yahoo.com>
2023-09-09 05:37:50 +0100

    Deciphering the atoi scroll

--- strn_2_int.c
+++ strn_2_int.c
@@ -12,3 +12,39 @@
  *
  * Return: The integer value represented by the input string.
  */
+int _atoi(char *s)
+{
+	int a;
+	int signal = 1;
+	int flags = 0;
+	int output;
+	unsigned int answer = 0;
+
+	for (a = 0; s[a] != '\0' && flags != 2; a++)
+	{
+		switch (s[a])
+		{
+			case '-':
+				signal *= -1;
+				break;
+			case '0': case '1': case '2': case '3': case '4':
+			case '5': case '6': case '7': case '8': case '9':
+				flags = 1;
+				answer *= 10;
+				answer += (s[a] - '0');
+				break;
+			default:
+				if (flags == 1)
+					flags = 2;
+				break;
+		}
+	}
+	if (signal == -1)
+		output = -answer;
+	else
+		output = answer;
+
+	return (output);
+}
+
+

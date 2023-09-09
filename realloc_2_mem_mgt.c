cec2db952ccd29dce6f0612cab32e4874068293d
CletsyMedia <Cletussam12@yahoo.com>
2023-09-09 14:58:15 +0100

    Unlocking memories

--- realloc_2_mem_mgt.c
+++ realloc_2_mem_mgt.c
@@ -1,1 +1,10 @@
 #include "shellux.h"
+
+
+/**
+ * free_mem - This frees a string of strings
+ * @pp: This is the string of strings
+ * The free_mem function deallocates memory for a string array, including
+ * its individual strings and the array itself. It takes a char ** pointer as
+ * input, iterates through the strings, and releases memory to prevent leaks.
+ */

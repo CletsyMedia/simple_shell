3cbd28c822d9a9ec754a5e040c4dedc424814e44
CletsyMedia <Cletussam12@yahoo.com>
2023-09-09 15:00:51 +0100

    reallocation & Unlocking memories

--- realloc_2_mem_mgt.c
+++ realloc_2_mem_mgt.c
@@ -22,3 +22,20 @@ void free_mem(char **pp)
 
 	free(a); /* Free the memory of the array itself */
 }
+
+/**
+ * reallocate_mem - reallocates a block of memory
+ * @ptr: pointer to previous malloc'ated block
+ * @old_mem: byte size of previous block
+ * @new_mem: byte size of new block
+ *
+ * This function reallocates a block of memory pointed to by @ptr.
+ * If @ptr is NULL, it behaves like malloc and allocates a new block of
+ * size @new. If @new is 0, it deallocates the memory block pointed to by @ptr
+ * and returns NULL. If the new size is the same as the old size, the function
+ * returns the original @ptr without changes. Otherwise, the function allocates
+ * a new block of size @new, copies the contents from the old block to the new
+ * block, and frees the old block.
+ *
+ * Return: Pointer to the reallocated memory block, or NULL on failure.
+ */

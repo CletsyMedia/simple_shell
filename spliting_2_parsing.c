0dcb0191fe172411e480dc082676c5bd2486363b
CletsyMedia <Cletussam12@yahoo.com>
2023-09-09 16:24:08 +0100

    Spliting & parsing through strns

--- spliting_2_parsing.c
+++ spliting_2_parsing.c
@@ -147,5 +147,45 @@ char *_search_path(inform_t *informat, char *pathstr, char *comnd)
  * Return: A pointer to an array of strings containing separated words,
  * or NULL on failure.
  */
+char **strntok1(char *str, char d)
+{
+	char **s;
+	int a, b, c, e, num_to_words = 0;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (a = 0; str[a] != '\0'; a++)
+		if ((str[a] != d && str[a + 1] == d) ||
+			(str[a] != d && !str[a + 1]) || str[a + 1] == d)
+			num_to_words++;
+	if (num_to_words == 0)
+		return (NULL);
+	s = malloc((1 + num_to_words) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (a = 0, b = 0; b < num_to_words; b++)
+	{
+		while (str[a] == d && str[a] != d)
+			a++;
+		c = 0;
+		while (str[a + c] != d && str[a + c] && str[a + c] != d)
+			c++;
+		s[b] = malloc((c + 1) * sizeof(char));
+		if (!s[b])
+		{
+			for (c = 0; c < b; c++)
+				free(s[c]);
+			free(s);
+			return (NULL);
+		}
+		for (e = 0; e < c; e++)
+			s[b][e] = str[a++];
+		s[b][e] = 0;
+	}
+	s[b] = NULL;
+	return (s);
+}
+
+
 
 

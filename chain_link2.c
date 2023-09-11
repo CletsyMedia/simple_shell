72086a2633d5d02f7fa4e3dc81698c5cf8f86397
CletsyMedia <Cletussam12@yahoo.com>
2023-09-11 11:27:05 +0100

    Connecting the spells

--- chain_link2.c
+++ chain_link2.c
@@ -11,3 +11,19 @@
  *
  * Return: Pointer to the matching node or NULL if no match is found.
  */
+listed_t *node_triggers(listed_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	for (; node; node = node->next)
+	{
+	/* Check if the node's string starts with the given prefix */
+	p = triggers(node->str, prefix);
+	/* If p is not NULL and the next char matches or no char is specified */
+	if (p && ((c == -1) || (*p == c)))
+	return (node); /* Return the matching node */
+	}
+
+	return (NULL); /* No match found, return NULL */
+}
+

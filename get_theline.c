ea0c4cde7ed2bfd567789498f6be61c6e761860d
CletsyMedia <Cletussam12@yahoo.com>
2023-09-11 06:06:15 +0100

    Getting that buffer line running

--- get_theline.c
+++ get_theline.c
@@ -1,1 +1,16 @@
 #include "shellux.h"
+
+
+
+/**
+ * sign_handlers - Handles the Ctrl-C signal
+ * @sign_num: The signal number (unused)
+ *
+ * This function is called when the Ctrl-C signal (SIGINT) is triggered.
+ * It prints a newline character and the shell prompt to create a new line.
+ * This behavior effectively blocks the default behavior of terminating the
+ * program when Ctrl-C is pressed. The `__attribute__((unused))` attribute is
+ * used to indicate that the parameter sign_num is not used in the function.
+ *
+ * Return: void
+ */

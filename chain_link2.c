14e171df9e99d469a7baf54ed20084abdf784508
CletsyMedia <Cletussam12@yahoo.com>
2023-09-11 11:26:10 +0100

    Connecting the spells

--- chain_link2.c
+++ chain_link2.c
@@ -1,1 +1,13 @@
 #include "shellux.h"
+
+
+
+/**
+ * node_triggers - Returns a node whose string starts with the given prefix
+ * and has the specified next character.
+ * @node: Pointer to the list head.
+ * @prefix: String to match as a prefix.
+ * @c: The next character after the prefix to match.
+ *
+ * Return: Pointer to the matching node or NULL if no match is found.
+ */

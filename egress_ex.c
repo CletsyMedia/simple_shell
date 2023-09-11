c1b7b365635198f7e8df09eb829f57ea1d2fdaf9
CletsyMedia <Cletussam12@yahoo.com>
2023-09-11 22:15:53 +0100

    The vanishing spell

--- egress_ex.c
+++ egress_ex.c
@@ -1,2 +1,10 @@
 #include "shellux.h"
 
+
+
+/**
+ * strn_char - Locates a character in a string up to a maximum length.
+ * @s: The string to be parsed.
+ * @c: The character to look for.
+ * Return: A pointer to the first occurrence of 'c' in 's', or NULL.
+ */

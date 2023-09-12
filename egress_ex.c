2e8c9df699528264587b6807da2fdc3b626b27f6
CletsyMedia <Cletussam12@yahoo.com>
2023-09-12 12:28:56 +0100

    The vanishing spell

--- egress_ex.c
+++ egress_ex.c
@@ -7,3 +7,17 @@
  * @c: The character to look for.
  * Return: A pointer to the first occurrence of 'c' in 's', or NULL.
  */
+
+char *strn_char(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	/* Return a pointer to the first occurrence of 'c' in 's' */
+	return (s);
+	s++; /* Increment s after the check*/
+	}
+	/* Return NULL if 'c' is not found in 's' */
+	return (NULL);
+}
+

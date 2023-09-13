dfe6a02a621e07f4a42649818ea6341a7e1bd52f
CletsyMedia <Cletussam12@yahoo.com>
2023-09-13 11:21:47 +0100

    Building the spell commandment

--- sh_comnd.c
+++ sh_comnd.c
@@ -6,3 +6,14 @@
  *
  * Return:	Always returns 0 to indicate successful execution.
  */
+int _shhelp(inform_t *informat)
+{
+	char **arg_arr;
+
+	arg_arr = informat->argv;
+	/* A message indicating that help functionality is not yet implemented */
+	_puts("The 'help' command is functional, but has not been implemented.\n");
+	if (0)
+		_puts(*arg_arr);
+	return (0);
+}

4de013ae9a8f3c875744e55c624be46fcf1252d0
CletsyMedia <Cletussam12@yahoo.com>
2023-09-13 11:25:49 +0100

    Aliases rebuild

--- sh_comnd2.c
+++ sh_comnd2.c
@@ -16,3 +16,35 @@
  * Return:
  *	- Always 0, indicating successful execution of the function.
  */
+int _shaliases(inform_t *informat)
+{
+	char *p = NULL; /* Pointer to '=' character in an argument */
+	listed_t *node = NULL; /* Pointer to alias linked list */
+	int a = 0; /* Counter for arguments */
+
+	if (informat->argc == 1)
+	{
+		/* If no arguments, display existing aliases */
+		node = informat->alias;
+		while (node)
+		{
+			 /* Display the alias */
+			print_alias(node);
+			 /* Move to the next alias in the list */
+			node = node->next;
+		}
+		return (0); /* Return success */
+	}
+
+	for (a = 1; informat->argv[a]; a++)
+	{
+		 /* Check if argument contains '=' */
+		p = strn_char(informat->argv[a], '=');
+		if (p)/* Set new alias */
+			set_alias(informat, informat->argv[a]);
+		else /* Display existing alias */
+			print_alias(node_triggers(informat->alias, informat->argv[a], '='));
+	}
+
+	return (0); /* Return success */
+}

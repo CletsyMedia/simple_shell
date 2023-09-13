6c2f818c56cab198e313b804d673b63218c34fa6
CletsyMedia <Cletussam12@yahoo.com>
2023-09-13 11:21:14 +0100

    Building the spell commandment

--- sh_comnd.c
+++ sh_comnd.c
@@ -1,1 +1,8 @@
 #include "shellux.h"
+
+/**
+ * _shhelp - Displays help information for the shell
+ * @informat: Pointer to the inform_t struct containing command information
+ *
+ * Return:	Always returns 0 to indicate successful execution.
+ */

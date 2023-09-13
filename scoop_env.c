51a5cbc4dd906ccfdc7e3b54cc141e4109a9359e
CletsyMedia <Cletussam12@yahoo.com>
2023-09-13 05:11:21 +0100

    Searching the env plains

--- scoop_env.c
+++ scoop_env.c
@@ -17,3 +17,41 @@
  * 'informat->argc'. The function also calls 'replc_aliases' and 'replc_vars'
  * to replace aliases and variables in 'informat'.
  */
+void _get_inform(inform_t *informat, char **av)
+{
+	int a = 0; /* Initialize 'a' outside of the loop */
+	 /* Set the 'fname' member of 'inform_t' to av[0] */
+	informat->fname = av[0];
+
+	if (informat->arg)
+	{
+	 /* Tokenize 'arg' and assign to 'argv' */
+	informat->argv = strntok(informat->arg, " \t");
+
+	if (!informat->argv)
+	{
+	/* Allocate memory for 'argv' */
+	informat->argv = malloc(sizeof(char *) * 2);
+
+	if (informat->argv)
+	{
+	/* Duplicate 'arg' to 'argv[0]' */
+	informat->argv[0] = strn_duplicate(informat->arg);
+	/* Set the second element of 'argv' to NULL */
+	informat->argv[1] = NULL;
+	}
+	}
+	 /* Iterate through 'argv' until NULL is encountered */
+	while (informat->argv[a] != NULL)
+	{
+	a++; /* Increment 'a' */
+	}
+
+	informat->argc = a; /* Set 'argc' to the final value of 'a' */
+
+	 /* Call the 'change_aliases' function with 'informat' as an argument */
+	change_aliases(informat);
+	 /* Call the 'change_vars' function with 'informat' as an argument */
+	change_vars(informat);
+	}
+}

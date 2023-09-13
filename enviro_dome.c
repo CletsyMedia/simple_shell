127c458fda57d85ca335c01d971614b8d695f4f1
CletsyMedia <Cletussam12@yahoo.com>
2023-09-13 05:44:03 +0100

    The dome of envs

--- enviro_dome.c
+++ enviro_dome.c
@@ -1,2 +1,14 @@
 #include "shellux.h"
 
+
+/**
+ * _shunsetenviro - Removes an environment variable.
+ * @informat: Pointer to an `inform_t` structure containing potential arg.
+ *	Used to maintain a constant function prototype.
+ *
+ * This function removes one or more environment variables.
+ * It takes in the variable names as arguments and removes them from the
+ * environment, effectively unsetting them.
+ *
+ * Return: Always 0
+ */

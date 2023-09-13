444b9d3176ee30a743658e204dfa49f938e7c935
CletsyMedia <Cletussam12@yahoo.com>
2023-09-13 18:49:15 +0100

    The main spell for all

--- main.c
+++ main.c
@@ -15,3 +15,39 @@
  *
  * Return: 0 on success, 1 on error.
  */
+int main(int ac, char **av)
+{
+	inform_t informat[] = { INFORM_INIT };
+	int fd = 2;
+
+	int new_fd = fd + 3;
+
+	if (ac == 2)
+	{
+	new_fd = open(av[1], O_RDONLY);
+	if (new_fd == -1)
+	{
+	if (errno == EACCES)
+	exit(126);
+	if (errno == ENOENT)
+	{
+	_errputs(av[0]);
+
+	_errputs(": 0: Can't open this file");
+
+	_errputs(av[1]);
+	_errputchar('\n');
+	_errputchar(BUFFER_FLUSH);
+	exit(127);
+	}
+	return (EXIT_FAILURE);
+	}
+	informat->readfd = new_fd;
+	}
+	_pop_enviro_list(informat);
+
+	read_hstry(informat);
+	_exec_hsh(informat, av);
+
+	return (EXIT_SUCCESS);
+}

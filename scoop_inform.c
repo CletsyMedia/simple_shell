f9d94e5315b9adf0f496965d5c4fe378eaf4b997
CletsyMedia <Cletussam12@yahoo.com>
2023-09-16 18:12:38 +0100

    Set and unset the env path

--- scoop_inform.c
+++ scoop_inform.c
@@ -2,143 +2,130 @@
 
 
 /**
- * _setenviro - Initializes a new environment variable or modifies an existing
- * one.
- * @informat: Pointer to the inform_t struct containing command information.
- * @var: The string representing the environment variable property.
- * @value: The string representing the environment variable value.
+ * _get_inform - Initializes inform_t struct with command arguments
+ * @informat: Pointer to the inform_t struct
+ * @av: Argument vector (array of strings)
  *
- * This function either initializes a new environment variable with the
- * specified property and value or modifies an existing environment variable.
- * It updates the environment_changed flag to indicate that the environment has
- * been modified.
- *
- * Return: Always 0.
+ * This function sets up the fields of the provided info_t struct with command
+ * arguments. It takes a pointer to the info_t struct 'informat' and an
+ * argument vector 'ag'. The 'fname' field is assigned the value of the first
+ * element of 'ag'. If 'informat->arg' is not NULL, the function tokenizes it
+ * using 'strntok' with delimiters " \t" and assigns the resulting array to
+ * 'informat->argv'. If 'informat->argv' is NULL, memory is allocated for a
+ * two-element array and 'informat->arg' is duplicated into
+ * 'informat->argv[0]'. The loop counts the elements in 'informat->argv' to set
+ * 'informat->argc'. The function also calls 'replc_aliases' and 'replc_vars'
+ * to replace aliases and variables in 'informat'.
  */
-int _setenviro(inform_t *informat, char *var, char *value)
+void _get_inform(inform_t *informat, char **av)
 {
-	char *buffs = NULL; /* Initialize a buffer to hold the variable and value. */
-	listed_t *node;	/* Create a pointer to traverse the linked list. */
-	char *p;	/* Create a pointer for searching within strings. */
-	/* Check if either 'var' or 'value' is NULL, and return 0 on failure. */
-	/* Check if either 'var' or 'value' is NULL, and return on failure. */
-	if (!var || !value)
-	return (0);
-	/* Allocate memory for 'buffs' to store the variable and value. */
-	buffs = malloc(_strn_length(var) + _strn_length(value) + 2);
-	if (!buffs)
-	return (1);
-	/* Copy 'var' into 'buffs'. */
-	_strn_copy(buffs, var);
-	/* Concatenate '=' to 'buffs'. */
-	_strn_concat(buffs, "=");
-	/* Concatenate 'value' to 'buffs'. */
-	_strn_concat(buffs, value);
-	/* Traverse the linked list 'informat->env'. */
-	for (node = informat->env; node; node = node->next)
-	{/* Search for 'var' within the current node's string. */
-	p = triggers(node->str, var);
-	/* If 'var' is found and followed by '=', update the value. */
-	if (p && *p == '=')
-	{/* Free the old string and replace it with 'buffs'. */
-	free(node->str);
-	node->str = buffs;
-	/* Set a flag to indicate that the environment was changed. */
-	informat->env_changed = 1;
-	/* Free 'buffs' as it's no longer needed. */
-	free(buffs);
-	return (0);/* Return success. */
+	int a = 0; /* Initialize 'a' outside of the loop */
+	 /* Set the 'fname' member of 'inform_t' to av[0] */
+	informat->fname = av[0];
+
+	if (informat->arg)
+	{
+	 /* Tokenize 'arg' and assign to 'argv' */
+	informat->argv = strntok(informat->arg, " \t");
+
+	if (!informat->argv)
+	{
+	/* Allocate memory for 'argv' */
+	informat->argv = malloc(sizeof(char *) * 2);
+
+	if (informat->argv)
+	{
+	/* Duplicate 'arg' to 'argv[0]' */
+	informat->argv[0] = strn_duplicate(informat->arg);
+	/* Set the second element of 'argv' to NULL */
+	informat->argv[1] = NULL;
 	}
 	}
-	/* If 'var' was not found in the linked list, add a new node at the end. */
-	addNode_end(&(informat->env), buffs, 0);
-	/* Set a flag to indicate that the environment was changed. */
-	informat->env_changed = 1;
-	/* Free 'buffs' as it's no longer needed. */
-	free(buffs);
-	return (0);/* Return success. */
+	 /* Iterate through 'argv' until NULL is encountered */
+	while (informat->argv[a] != NULL)
+	{
+	a++; /* Increment 'a' */
+	}
+
+	informat->argc = a; /* Set 'argc' to the final value of 'a' */
+
+	 /* Call the 'change_aliases' function with 'informat' as an argument */
+	change_aliases(informat);
+	 /* Call the 'change_vars' function with 'informat' as an argument */
+	change_vars(informat);
+	}
 }
-
-
 
 
 /**
- * _getenviro - Retrieves and returns a copy of the environment variables.
- * @informat: Pointer to the inform_t struct containing command information.
+ * _clr_inform - Initializes an inform_t struct
+ * @informat: Pointer to the inform_t struct to be initialized
  *
- * This function fetches a copy of the environment variables from the inform_t
- * structure. If changes were made to the environment or if the copy hasn't
- * been retrieved yet, it updates the stored copy using the listTostrns
- * function.
- *
- * Return: A char pointer to the string array containing environment variables.
+ * This function sets the fields of the provided inform_t struct to their
+ * initial values. It initializes the 'arg', 'argv', 'path', and 'argc' fields,
+ * ensuring that the struct is properly prepared for subsequent usage.
  */
-char **_getenviro(inform_t *informat)
+void _clr_inform(inform_t *informat)
 {
+	/* Set the 'arg' member of the 'inform_t' structure to NULL */
+	informat->arg = NULL;
 
-	if (!informat->environ || informat->env_changed)
-	{
-	/*Convert the 'env' linked list to an array of strings ('environ')*/
-	informat->environ = listTostrns(informat->env);
+	/* Set the 'arg' member of the 'inform_t' structure to NULL */
+	informat->argv = NULL;
 
-	/* This reset the env_change flag*/
-	informat->env_changed = 0;
+	/* Set the 'path' member of the 'inform_t' structure to NULL */
+	informat->path = NULL;
 
-	}
-	/*Return the 'environ' array.*/
-	return (informat->environ);
+	/* Set the 'arg' member of the 'inform_t' structure to 0 */
+	informat->argc = 0;
 }
-
 /**
- * _unsetenviro - Removes an environment variable from the list.
- * @informat: Pointer to the inform_t struct containing command information.
- * @var: The string representing the environment variable to remove.
+ * _free_inform - Frees inform_t struct fields
+ * @informat: Pointer to the parameter to the inform_t struct
+ * @all: Flag to indicate if all fields should be freed (1: yes, 0: no)
  *
- * This function iterates through the list of environment variables stored in
- * the inform_t structure and removes the environment variable indicated by
- * the var parameter. The environment_changed flag is updated to reflect the
- * change.
- *
- * Return: 1 if the environment variable was successfully removed, 0 otherwise.
+ * This function deallocates memory associated with various fields of the
+ * provided info_t struct 'informat'. It takes a pointer to the struct and
+ * a boolean flag 'all'. If 'all' is true, it frees additional fields including
+ * 'cmd_buf', 'env', 'history', 'alias', 'environ', and closes 'readfd' if it's
+ * greater than 2. Otherwise, it only frees 'argv' and sets 'path' to NULL.
  */
-int _unsetenviro(inform_t *informat, char *var)
-{
-	char *p;
-	listed_t *node = informat->env; /* A pointer to traverse the linked list. */
-	size_t a = 0; /* Initialize an index counter. */
+void _free_inform(inform_t *informat, int all)
+{/* Free memory allocated for 'argv' */
+	free_mem(informat->argv);
+	informat->argv = NULL;
+	informat->path = NULL;
 
-	/* Check if the linked list or 'var' is NULL, and return 0 for NULL. */
-	if (!node || !var)
-		return (0);
-
-	/* Loop through the linked list. */
-	while (node)
-	{
-		/* Search for 'var' within the current node's string. */
-		p = triggers(node->str, var);
-		/* If 'var' is found and followed by '=', remove the node at index 'a'. */
-		if (p && *p == '=')
-		{
-			/* Remove the node at index 'a' and update 'informat->env_changed'. */
-			informat->env_changed = del_node_idx(&(informat->env), a);
-
-			/* Reset the index counter to 0. */
-			a = 0;
-
-			/* Reset 'node' to the beginning of the linked list. */
-			node = informat->env;
-
-			/* Continue checking the remaining nodes. */
-			continue;
-		}
-		/* Move to the next node in the linked list. */
-		node = node->next;
-		/* Increment the index counter. */
-		a++;
+	if (all)/* If 'all' flag is true */
+	{/* If 'cmd_buffs' is not allocated */
+	if (!informat->cmd_buffs)
+	/* Free memory allocated for 'arg' */
+	free(informat->arg);
+	/* If 'env' is allocated */
+	if (informat->env)
+	 /* Free memory for 'env' linked list */
+	_free_list(&(informat->env));
+	/* If 'history' is allocated */
+	if (informat->history)
+	/* Free memory for 'history' linked list */
+	_free_list(&(informat->history));
+	 /* If 'alias' is allocated */
+	if (informat->alias)
+	 /* Free memory for 'alias' linked list */
+	_free_list(&(informat->alias));
+	/* Free memory allocated for 'environ' */
+	free_mem(informat->environ);
+	 /* Set 'environ' to NULL */
+	informat->environ = NULL;
+	/* Free memory allocated for 'cmd_buffs' array */
+	buffs_free((void **)informat->cmd_buffs);
+	/* If 'readfd' is greater than 2 */
+	if (informat->readfd > 2)
+	/* Close the file descriptor 'readfd' */
+	close(informat->readfd);
+	/* Write a character to flush the buffer */
+	_putchar(BUFFER_FLUSH);
 	}
-	/* Return the value of 'informat->env_changed' */
-	return (informat->env_changed);
 }
-
 
 
